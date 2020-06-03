3237fd2c0361d832024deb92aad02a73fcfaf0be
Dania King <dania@fortunebuilders.com>
2020-06-02 21:44:36 -0700

    added picture links and some update style

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component
   {
     return (
       <Wrapper>
-        <Title>Clicky Game</Title>
+        <Title> Boondocks Memory Game </Title>
         {this.state.friends.map( friend => (
           <FriendCard
 
